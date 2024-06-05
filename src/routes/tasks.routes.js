0f8d248c29180a2c9976e98b04f747fce9b359b9
facundo1996 <ffaacu1@gmail.com>
2024-06-05 09:17:42 -0300

    Add validator

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { authRequired } from '../middlewares/validateToken.js';
 import { getTask, getTasks, updateTask, deleteTask, createTask } from "../controllers/tasks.controller.js";
-
+import { createTaskSchema } from "../schemas/task.schema.js"
+import { validateSchema } from "../middlewares/validator.middleware.js";
 
 const router = Router()
 
 router.get('/tasks', authRequired, getTasks )
-router.post('/tasks', authRequired, createTask )
+router.post(
+  '/tasks', 
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTask
+)
 router.get('/tasks/:id', authRequired, getTask )
 router.delete('/tasks/:id', authRequired, deleteTask )
 router.put('/tasks/:id', authRequired, updateTask )
